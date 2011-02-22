2815018bf5242d31a09db8cc0b4c61c7ae533cec
Kurt- <kurt.hogentogler@gmail.com>
2011-02-22 16:04:50 -0500

    Added help action and help page also added rspec before(:each) do facility which executes a block of code before each test case; defined a @base_title instance variable that eliminates duplication in specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,42 +1,59 @@
 require 'spec_helper'
 
 describe PagesController do
-  render_views
-
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-    it "should have the right title" do 
-      get 'home' 
-      response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Micro Blog | Home")
-    end
-  end
-
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-    it "should have the right title" do 
-      get 'contact'
-      response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Micro Blog | Contact")
-    end
-  end
-  
-  describe "GET 'about'" do 
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
-    it "should have the right title" do 
-      get 'about'
-      response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Micro Blog | About")
-    end
+   render_views
+   
+   before(:each) do 
+     @base_title = "Ruby on Rails Tutorial Micro Blog"
    end
+   
+  
+      describe "GET 'home'" do
+        it "should be successful" do
+          get 'home'
+          response.should be_success
+        end
+        it "should have the right title" do 
+          get 'home' 
+          response.should have_selector("title",
+                                        :content => @base_title + " | Home")
+        end
+      end
+    
+      describe "GET 'contact'" do
+        it "should be successful" do
+          get 'contact'
+          response.should be_success
+        end
+        it "should have the right title" do 
+          get 'contact'
+          response.should have_selector("title",
+                                        :content => @base_title + " | Contact")
+        end
+      end
+      
+      describe "GET 'about'" do 
+        it "should be successful" do
+          get 'about'
+          response.should be_success
+        end
+        it "should have the right title" do 
+          get 'about'
+          response.should have_selector("title",
+                                       :content => @base_title + " | About")
+        end
+       end
+   
+   describe "GET 'help'" do
+     it "should be successful" do
+       get 'help'
+       response.should be_success
+     end
+     it "should have the right title" do 
+        get 'help'
+        response.should have_selector("title",
+                                      :content => @base_title + " | Help")
+      end
+    end
 
 end
